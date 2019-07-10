b58a0352d0b16a81f30efb6aa46f7cc5be7bef3a
tahamazari <tahamazari@gmail.com>
2019-07-10 13:40:24 +0500

    Node: CORS passing frontend url

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ const users_router = require('./routes/user_routes.js')
 const films_router = require('./routes/film_routes.js')
 const ratings_router = require('./routes/rating_routes.js')
 
+var whitelist = [
+  'https://react-spa-tintash.herokuapp.com',
+];
+var corsOptions = {
+  origin: function(origin, callback){
+      var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+      callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
 
-
-app.use(cors())
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -23,11 +32,11 @@ app.use(films_router)
 app.use(ratings_router)
 
 
-app.use(function(request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+// app.use(function(request, response, next) {
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
+// });
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
